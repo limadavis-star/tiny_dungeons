0affaf8188cfd4cdaa9baf99d399a13184b671fd
Davi Lima <davilimadasilva@proton.me>
2026-09-10 15:52:35 -0300

    chore: configure game window

---
+++ conf.lua
@@ -0,0 +1,12 @@
+function love.conf(configuration)
+    configuration.identity = "tiny_dungeons"
+    configuration.version = "11.5"
+
+    configuration.window.title = "Tiny Dungeons"
+    configuration.window.width = 1366
+    configuration.window.height = 768
+    configuration.window.resizable = true
+    configuration.window.vsync = 1
+
+    configuration.console = false
+end
