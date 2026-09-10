cfb74ebb3abbce39dae4937e13fa07ebbfeed1f0
Davi Lima <davilimadasilva@proton.me>
2026-09-10 16:49:16 -0300

    fix: move drawing out of love load

--- main.lua
+++ main.lua
@@ -7,9 +7,6 @@ function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.graphics.setBackgroundColor(0.02, 0.04, 0.10)
 
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.print("Tiny Dungeons", 32, 32)
-
     sceneManager:hook()
     sceneManager:enter(Gameplay)
 end
