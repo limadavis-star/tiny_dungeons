2db9d1c1b34dbf31ff6d25f45a0e1b373b934f8b
Davi Lima <davilimadasilva@proton.me>
2026-09-10 16:44:34 -0300

    feat: support gamepad movement

--- src/input/controls.lua
+++ src/input/controls.lua
@@ -5,29 +5,43 @@ local controls = baton.new({
         left = {
             "key:a",
             "key:left",
+            "axis:leftx-",
+            "button:dpleft",
         },
 
         right = {
             "key:d",
             "key:right",
+            "axis:leftx+",
+            "button:dpright",
         },
 
         up = {
             "key:w",
             "key:up",
+            "axis:lefty-",
+            "button:dpup",
         },
 
         down = {
             "key:s",
             "key:down",
+            "axis:lefty+",
+            "button:dpdown",
         },
-
-
     },
 
     pairs = {
-        move = { "left", "right", "up", "down" }
-    }
+        move = {
+            "left",
+            "right",
+            "up",
+            "down",
+        },
+    },
+
+    joystick = love.joystick.getJoysticks()[1],
+    deadzone = 0.15,
 })
 
 return controls
