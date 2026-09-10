73f3c8d3f860f969163634f3a7b6f8d6a0e87ebd
Davi Lima <davilimadasilva@proton.me>
2026-09-10 16:19:38 -0300

    feat: manage gameplay scene with roomy

--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
+local roomy = require("libraries.roomy.roomy")
+local Gameplay = require("src.states.gameplay")
+
+local sceneManager = roomy.new()
+
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.graphics.setBackgroundColor(0.02, 0.04, 0.10)
-end
 
-function love.update(dt)
-end
-
-function love.draw()
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.print("Tiny Dungeons", 32, 32)
+
+    sceneManager:hook()
+    sceneManager:enter(Gameplay)
 end
