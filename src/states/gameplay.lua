91bc35b3bd76f74fc86421a46ca1f2d45be52710
Davi Lima <davilimadasilva@proton.me>
2026-09-10 16:26:59 -0300

    feat: create gameplay ecs world

--- src/states/gameplay.lua
+++ src/states/gameplay.lua
@@ -1,6 +1,17 @@
+local Concord = require("libraries.concord")
+
 local Gameplay = {}
 
+function Gameplay:enter()
+    self.ecsWorld = Concord.world()
+end
+
+function Gameplay:update(dt)
+    self.ecsWorld:emit("update", dt)
+end
+
 function Gameplay:draw()
+    self.ecsWorld:emit("draw")
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.print("Tiny Dungeons", 32, 32)
 end
