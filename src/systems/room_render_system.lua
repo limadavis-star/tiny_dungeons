c6636e09fd9068810bd926e1c95d6e341e403507
Davi Lima <davilimadasilva@proton.me>
2026-09-13 14:37:26 -0300

    feat: draw fixed room boundary

--- src/systems/room_render_system.lua
+++ src/systems/room_render_system.lua
@@ -10,16 +10,26 @@ local RoomRenderSystem = Concord.system({
 function RoomRenderSystem:draw()
     love.graphics.setColor(0.08, 0.12, 0.22, 1)
 
+
     for _, entity in ipairs(self.pool) do
+        local position = entity.position
+        local room = entity.room
+
         love.graphics.rectangle(
             "fill",
-            entity.position.x,
-            entity.position.y,
-            entity.room.width,
-            entity.room.height
+            position.x,
+            position.y,
+            room.width,
+            room.height
         )
+
+        love.graphics.setColor(0.34, 0.48, 0.65, 1)
+        love.graphics.setLineWidth(8)
+        love.graphics.rectangle("line", position.x, position.y, room.width, room.height)
     end
 
+
+    love.graphics.setLineWidth(1)
     love.graphics.setColor(1, 1, 1, 1)
 end
 
